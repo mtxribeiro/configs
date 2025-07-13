5ad69d1e9d067590c0fa8960f971add400218f3d
mtxribeiro <matheusribeirovivas06@gmail.com>
2025-07-13 13:48:31 -0300

    blabla

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+sudo pacman -S --needed hyprland hyprpaper hyprlock waybar tofi kitty
+
+command -v yay &>/dev/null || (cd /tmp && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm && cd ~ && rm -rf /tmp/yay)
+
+yay -S tofi
+
 cp -rf .config ~/
 cp .bashrc ~/ 2>/dev/null || true
 mkdir -p ~/Imagens && cp -rf Wallpapers ~/Imagens/
+
 echo -e "\n\033[1;32mConfigurações restauradas com sucesso!\033[0m"
