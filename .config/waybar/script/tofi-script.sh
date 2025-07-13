2cbd76f977adf5cc38f2e441bec0acfccd27eacb
mtxribeiro <matheusribeirovivas06@gmail.com>
2025-07-13 13:21:26 -0300

    att

--- .config/waybar/script/tofi-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pkill waybar
-waybar -c ~/.config/waybar/waybar-tofi/config.jsonc -s ~/.config/waybar/waybar-tofi/style.css &
-tofi-drun --drun-launch true &
-pid=$!
-wait $pid
-pkill waybar
-waybar &
